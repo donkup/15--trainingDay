0fc4b4e42be40219e446f4d829e10479197466c7
Donatas <donkuprys@gmail.com>
2021-05-24 18:52:47 +0300

    sum testst jest

---
+++ js/sum/sum.test.js
@@ -0,0 +1,14 @@
+const sum = require('./sum');
+
+test('adds 0 + 0 to equal 0', () => {
+    expect(sum(0, 0)).toBe(0);
+});
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 0.5 + 0.5 to equal 1 ', () => {
+    expect(sum(0.5, 0.5)).toBeCloseTo(1, 5);
+});
+
